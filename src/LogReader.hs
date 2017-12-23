2bbb9b8e4a032e9d6131a2b5c7992cbe605fc964
ba205 <basilahmad25@yahoo.com>
2017-12-22 21:10:14 -0500

    Main method finished

--- src/LogReader.hs
+++ src/LogReader.hs
@@ -8,24 +8,29 @@ import qualified Data.PQueue.Max as PM
 import Data.Text (Text)
 import qualified Data.Text as T
 import Control.Monad.Trans.Except
+import Data.Time.Clock
+import Data.Time.Calendar
+import GHC.Conc
 
 data LogMessage = LogMessage { message    :: Text
                              , priority   :: Int
-                             , date       :: Text
-                             , time       :: Text
+                             , datetime   :: Text
                              , threadName :: Text
-                             , moduleName :: Text
                              } deriving Show
 
-createLogMessage :: Text -> Int -> LogMessage
+createLogMessage :: Text -> Int -> IO LogMessage
 createLogMessage message priority =
-  let date = undefined
-      time = undefined
-      threadName = undefined
-      moduleName = undefined
-    in LogMessage message priority date time threadName moduleName
+  do
+    datetime <-  getCurrentTime
+    let datetime' = T.pack $ show datetime
+    threadName <- myThreadId
+    let threadName' = T.pack $ show threadName
+    return $ LogMessage message priority datetime' threadName'
 
-logReader :: Logger -> ExceptT LogError IO LogMessage 
+logReader :: Logger -> IO (Logger, LogMessage) 
 logReader (Logger masterLog queueBound defaultPriority) =
   do
-    undefined 
+    let ((priority, message), logger) = PM.deleteFindMax masterLog
+    logMessage <- createLogMessage message priority
+    let newLogger = Logger logger queueBound defaultPriority
+    return (newLogger, logMessage)
--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Logger
 import LogReader
+import Data.Text as T
 
+-- | Helper function
+fromEither :: Either a b -> b
+fromEither (Right x) = x
+
+-- | Examples of expected behavior.
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  let logger = createLogger
+
+      -- example given in prompt
+      l1 = updateLogger 1 "abc" logger
+      l2 = updateLogger 2 "def" (fromEither l1)
+      l3 = updateLogger 1 "ghi" (fromEither l2)   
+
+  case l3 of
+    Left error -> putStrLn $ T.unpack error
+    Right l    -> do
+      (log1, m1) <- logReader l
+      putStrLn $ "test: 'def' is highest priority" ++ show m1
+      (log2, m2) <- logReader log1
+      putStrLn $ "test: 'ghi' is next highest priority" ++ show m2
+      (log3, m3) <- logReader log2
+      putStrLn $ "test: 'abc' is next highest priority" ++ show m3
+      -- expect exception, since empty queue
+      (log4, m4) <- logReader log3
+      putStrLn $ "Exception expected"
+      putStrLn $ show m4
